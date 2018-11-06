0529ef48e78cb1fed9698d37996a793d5a342b8f
Jossué Dután <jossuedutan@gmail.com>
2018-11-06 15:51:50 -0500

    TypeScript y Observables intro

---
+++ 02-Typescript/02-observables.js
@@ -0,0 +1,14 @@
+// 02-observables.ts
+const rxjs = require('rxjs');
+const observableNumeros$ = rxjs.of(1, 2, 3, 4, 5, 6);
+observableNumeros$
+    .subscribe((ok) => {
+    console.log('ok', ok);
+}, //then
+(error) => {
+    console.log('error', error);
+}, //then
+() => {
+    console.log('complete');
+} //then
+);
