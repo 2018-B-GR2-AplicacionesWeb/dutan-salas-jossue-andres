d6fb1b447c628bc46d89c49d1b9e70d8eed6f5ef
Jossué Dután <jossuedutan@gmail.com>
2018-11-07 15:58:05 -0500

    Observables fin

--- 02-Typescript/02-observables.js
+++ 02-Typescript/02-observables.js
@@ -1,14 +1,56 @@
 // 02-observables.ts
-const rxjs = require('rxjs');
-const observableNumeros$ = rxjs.of(1, 2, 3, 4, 5, 6);
+var rxjs = require('rxjs');
+var map = require('rxjs/operators').map; //Importar el map
+var distinct = require('rxjs/operators').distinct; //Para que los observables no se repitan
+var concat = require('rxjs/operators').concat;
+// const observableNumeros$ = rxjs.of(1, 2, 3, 4, 5, 6);
+var observableNumeros$ = rxjs.of(1, true, 2, 'Adrian', 3, { nombre: 'Adrian' }, ['oli'], 2, function () {
+});
 observableNumeros$
-    .subscribe((ok) => {
+    .pipe(distinct(), map(function (valorActual) {
+    return {
+        data: valorActual
+    };
+}))
+    .subscribe(function (ok) {
     console.log('ok', ok);
 }, //then
-(error) => {
+function (error) {
     console.log('error', error);
 }, //then
-() => {
+function () {
     console.log('complete');
 } //then
 );
+var promesita = function (funciona) {
+    return new Promise(function (resolve, reject) {
+        if (funciona) {
+            resolve(' :) ');
+        }
+        else {
+            reject(' :( ');
+        }
+    });
+};
+var promesita$ = rxjs.from(promesita(true));
+promesita$
+    .subscribe(function (ok) {
+    console.log('Promesita bien ', ok);
+}, function (error) {
+    console.log('Promesita mal ', error);
+}, function () {
+    console.log('Completado');
+});
+var observableConcatenado$ = observableNumeros$
+    .pipe(concat(promesita$), distinct(), map(function (valorActual) {
+    return {
+        data: valorActual
+    };
+}));
+observableConcatenado$.subscribe(function (ok) {
+    console.log('Concatenado bien ', ok);
+}, function (error) {
+    console.log('Concatenado mal ', error);
+}, function () {
+    console.log('Completado');
+});
--- 02-Typescript/02-observables.ts
+++ 02-Typescript/02-observables.ts
@@ -1,11 +1,37 @@
 // 02-observables.ts
 
 declare var require: any;
-const rxjs = require('rxjs');
 
-const observableNumeros$ = rxjs.of(1, 2, 3, 4, 5, 6);
+const rxjs = require('rxjs');
+const map = require('rxjs/operators').map; //Importar el map
+const distinct = require('rxjs/operators').distinct; //Para que los observables no se repitan
+const concat = require('rxjs/operators').concat;
+
+// const observableNumeros$ = rxjs.of(1, 2, 3, 4, 5, 6);
+const observableNumeros$ = rxjs.of(1,
+    true,
+    2,
+    'Adrian',
+    3,
+    {nombre: 'Adrian'},
+    ['oli'],
+    2,
+    function () {
+
+    }
+);
 
 observableNumeros$
+    .pipe(
+        distinct(),
+        map(
+            (valorActual) => {
+                return {
+                    data: valorActual
+                }
+            }
+        )
+    )
     .subscribe(
         (ok) => {
             console.log('ok', ok)
@@ -16,5 +42,60 @@ observableNumeros$
         () => {
             console.log('complete')
         } //then
+    );
+
+const promesita = (funciona: boolean): Promise<string> => {
+    return new Promise(
+        (resolve, reject) => {
+            if (funciona) {
+                resolve(' :) ')
+            }
+            else {
+                reject(' :( ')
+            }
+        }
+    )
+};
+
+const promesita$ = rxjs.from(promesita(true));
+
+promesita$
+    .subscribe(
+        (ok) => {
+            console.log('Promesita bien ', ok)
+        },
+        (error) => {
+            console.log('Promesita mal ', error)
+
+        },
+        () => {
+            console.log('Completado')
+        }
+    );
+
+const observableConcatenado$ = observableNumeros$
+    .pipe(
+        concat(promesita$),
+        distinct(),
+        map(
+            (valorActual) => {
+                return {
+                    data: valorActual
+                }
+            }
+        )
     )
 ;
+
+observableConcatenado$.subscribe(
+    (ok) => {
+        console.log('Concatenado bien ', ok)
+    },
+    (error) => {
+        console.log('Concatenado mal ', error)
+
+    },
+    () => {
+        console.log('Completado')
+    }
+);
